5da1468ad0c340979f002888e5528f1e353b1869
Edgar <egblanco.betasoft@gmail.com>
2023-05-22 11:16:49 -0500

    changes

--- src/pages/api/products.json/index.js
+++ src/pages/api/products.json/index.js
@@ -1,4 +1,23 @@
-export default function handler(req, res) {
+import Cors from "cors";
+
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
   if (req.method === "GET") {
     res.status(200).json({ data });
   } else {
